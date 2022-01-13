05835f8a741e79749028d0a8b8c4610a4f7520d6
Srivatsan Talkad <stalkad01@gmail.com>
2022-01-13 13:36:55 -0500

    Create index.html, style.css, script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+// The book array will be a global var.
+const bookLibrary = [];
+
+// Create reference to add book button, and the books container.
+const addBook = document.querySelector(".add-book");
+const booksContainer = document.querySelector(".books-container");
+
+// Create event listener for add book
+addBook.addEventListener('click', () => {
+    addBookToLibrary();    
+});
+
+// Another global var is the book object constructor.
+function Book(title, author, numOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.read = read;
+}
+
+// This function will create a book object, provided the user input, and return it.
+function createBook() {
+    // Get user input.
+    let bookInfo = getUserInput();
+
+    // Add book to bookLibrary global var.
+    newBook = new Book(bookInfo[0], bookInfo[1], bookInfo[2], bookInfo[3]);
+
+    return newBook;
+}
+
+// This function will display the newly added book to the page.
+function displayBook(newBook) {
+    // Create new divs that will contain the metadeta.
+    let bookDiv = document.createElement('div');
+    let bookTitleDiv = document.createElement('div');
+    let bookAuthorDiv = document.createElement('div');
+    let bookPagesDiv = document.createElement('div');
+    let bookReadDiv = document.createElement('div');
+
+    // Set the divs to its corresponding text.
+    bookTitleDiv.innerText = "Title: " + (newBook.title == "" ? "N/A" : newBook.title);
+    bookAuthorDiv.innerText = "Author: " + (newBook.author == "" ? "N/A" : newBook.author);
+    bookPagesDiv.innerText = "# of Pages: " + (newBook.numOfPages == "" ? "N/A" : newBook.numOfPages);
+    bookReadDiv.innerText = "Read: " + (newBook.read == "" ? "N/A" : newBook.read);
+
+    bookDiv.appendChild(bookTitleDiv);
+    bookDiv.appendChild(bookAuthorDiv);
+    bookDiv.appendChild(bookPagesDiv);
+    bookDiv.appendChild(bookReadDiv);
+
+
+    // Create new button that will be used to remove the book from the list.
+    let removeBook = document.createElement('button');
+    removeBook.innerText = "Remove Book";
+    bookDiv.appendChild(removeBook);
+    removeBook.addEventListener('click', () => {
+        booksContainer.removeChild(bookDiv);
+    });
+
+    bookDiv.style.cssText = "display:flex; flex-direction: column; justify-content: center; align-items: center; border: 1px solid black; border-radius: 20px; margin: 1em; padding: 1em; gap: 1em";
+    booksContainer.appendChild(bookDiv);
+}
+
+// This function gets user input from form and returns user input.
+function getUserInput() {
+    // Prompt user for book title.
+    let bookTitle = document.querySelector('#book-title').value;
+
+    // Prompt user for book author.
+    let bookAuthor = document.querySelector('#book-author').value;
+
+    // Prompt user for number of pages.
+    let numberOfPages = document.querySelector('#book-pages').value;
+
+    // Prompt user for whether they have read the book
+    let bookRead = document.querySelector('#book-read').value;
+
+    // Clear contents of form
+    document.querySelector('#book-title').value = "";
+    document.querySelector('#book-author').value = "";
+    document.querySelector('#book-pages').value = "";
+    document.querySelector('#book-read').value = "";
+
+    return [bookTitle, bookAuthor, numberOfPages, bookRead];
+}
+
+// This function stores a book object into global var "bookLibrary".
+function addBookToLibrary() {
+    let newBook = createBook();
+    bookLibrary.push(newBook);
+    displayBook(newBook);
+}
+
+
+
+
+
